752fd1156c7148e6e70c626a747b774a1ee8c046
Serge <serge-mur@yandex.ru>
2024-09-03 16:55:52 +0300

    structure

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const mainSliderThumbs = new Swiper(".main-slider__thumbs", {
     // loop: true,
-    spaceBetween: 10,
+    // spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     // watchSlidesProgress: true,
+    breakpoints: {
+      // when window width is >= 576px
+      576: {
+        slidesPerView: 5, 
+      },
+    }
   });
   const mainSliderContent = new Swiper(".main-slider__content", {
     // loop: true,
