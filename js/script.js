399cf77472ef8cc29173fde8516baed716b29e63
Serge <serge-mur@yandex.ru>
2024-09-04 17:35:46 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -1,40 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   const mainSliderThumbs = new Swiper(".main-slider__thumbs", {
-    // loop: true,
-    // spaceBetween: 10,
     slidesPerView: 3.5,
     freeMode: true,
-    // freeMode: {
-    //   enabled: true,
-    //   sticky: true,
-    // },
-    // centeredSlides: true,
-    // centeredSlidesBounds: true,
-    // centerInsufficientSlides: true,
-    // watchSlidesProgress: true,
     breakpoints: {
-      // when window width is >= 576px
+      //  >= 576px
       576: {
         slidesPerView: 5, 
       },
     }
   });
+
   const mainSliderContent = new Swiper(".main-slider__content", {
-    slidesPerView: 1.025,
-    // loop: true,
-    // spaceBetween: 0,
-    // navigation: {
-    //   nextEl: ".swiper-button-next",
-    //   prevEl: ".swiper-button-prev",
-    // },
+    slidesPerView: 'auto',
+    thumbs: {
+      swiper: mainSliderThumbs,
+      autoScrollOffset: 1
+    },    
     breakpoints: {
-      // when window width is >= 576px
-      992: {
+      576: {
         slidesPerView: 1, 
       },
     },
-    thumbs: {
-      swiper: mainSliderThumbs,
-    },
   });
+
 });
